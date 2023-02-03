2f610147487bbea59262cf36fbf410b00b6d403a
Mads Andreasen <github@andreasen.cc>
2023-02-03 14:07:08 +0100

    Puts BMP in bootloader mode, if present

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::{path::PathBuf, time::Duration};
 
 use clap::Parser;
 
@@ -12,11 +12,45 @@ struct Args {
     probe: Option<bool>,
 
     file: Option<PathBuf>,
-
 }
 
-fn main() {
-    let args = Args::parse();
+// const STLINK_VID: u16 = 0x0483;
+// const STLINK_PID: u16 = 0x3748;
+// const STLINK_PIDV21: u16 = 0x374b;
+// const STLINK_PIDV21_MSD: u16 = 0x3752;
+// const STLINK_PIDV3_MSD: u16 = 0x374e;
+// const STLINK_PIDV3: u16 = 0x374f;
+// const STLINK_PIDV3_BL: u16 = 0x374d;
 
-    println!("Hello, world!");
+const OPENMOKO_VID: u16 = 0x1d50;
+const BMP_APPL_PID: u16 = 0x6018;
+
+const BMP_DFU_IF: u8 = 4;
+
+fn main() {
+    // let args = Args::parse();
+
+    for device in rusb::devices().unwrap().iter() {
+        let device_desc = device.device_descriptor().unwrap();
+
+        if device_desc.vendor_id() == OPENMOKO_VID && device_desc.product_id() == BMP_APPL_PID {
+            println!("Found BMP. Switching to bootloader");
+
+            match device.open() {
+                Ok(mut handle) => {
+                    let buf: [u8; 0] = [];
+                    handle.claim_interface(BMP_DFU_IF).unwrap();
+                    handle.write_control(
+                        rusb::constants::LIBUSB_ENDPOINT_OUT | rusb::constants::LIBUSB_REQUEST_TYPE_CLASS | rusb::constants::LIBUSB_RECIPIENT_INTERFACE,
+                        0, /*DFU_DETACH,*/
+                        1000,
+                        BMP_DFU_IF as u16,
+                        &buf,
+                        Duration::from_millis(5000)).unwrap();
+                    handle.release_interface(0).unwrap();
+                },
+                Err(error) => println!("... Failed with error: {error}"),
+            }
+        }
+    }
 }
