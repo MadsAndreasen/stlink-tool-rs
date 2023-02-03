b6712213a98244a3cea43d90eb94fb0e30faf017
Mads Andreasen <github@andreasen.cc>
2023-02-03 11:16:34 +0100

    Added commandline arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+/// A tool to flash chinese ST-link dongles
+/// Application is started when called without argument or after firmware load
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Probe the ST-Link adapter
+    #[clap(short, long)]
+    probe: Option<bool>,
+
+    file: Option<PathBuf>,
+
+}
+
 fn main() {
+    let args = Args::parse();
+
     println!("Hello, world!");
 }
